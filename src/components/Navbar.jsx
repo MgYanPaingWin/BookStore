bc8c8bccf783d61695358d33e3a9f8ac9f2170ab
Yan Pai Win <yanpaiwin201916@gmail.com>
2025-08-20 21:26:42 +0630

    ep-55

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="border border-b-1">
+      <ul className="flex justify-between items-center p-3 max-w-6xl mx-auto">
+        <li className="flex items-center gap-3">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+            />
+          </svg>
+
+          <input
+            type="text"
+            placeholder="search books..."
+            className="outline-none"
+          />
+        </li>
+        <NavLink to="/" className="flex items-center gap-3 md:-ml-32 cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0 0 12 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75Z"
+            />
+          </svg>
+
+          <span className="text-2xl font-bold text-primary hidden md:block">
+            BookStore
+          </span>
+        </NavLink>
+
+        <li className="flex gap-3 i tems-center">
+          {/* create book */}
+          <NavLink
+            to="/create"
+            className="text-white bg-primary px-3 py-2 rounded-2xl flex items-center gap-1"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+            <span className="hidden md:block">Create book</span>
+          </NavLink>
+
+          {/* profile image */}
+          <div className="w-11">
+            <img
+              src="https://avatars.githubusercontent.com/u/142674055?v=4"
+              alt=""
+              className="w-full rounded-full"
+              srcset=""
+            />
+          </div>
+        </li>
+      </ul>
+    </nav>
+  );
+}
--- src/pages/layouts/Layout.jsx
+++ src/pages/layouts/Layout.jsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../../components/Navbar";
 
 export default function Layout() {
   return (
     <div>
-      <nav>
-        <ul>
-            <li>Home</li>
-            <li>Create</li>
-        </ul>
-      </nav>
-
+      <Navbar/>
+      
       {/* router */}
-      <Outlet/>
+      <div className="max-w-6xl mx-auto p-3">
+        <Outlet />
+      </div>
     </div>
-  )
+  );
 }
