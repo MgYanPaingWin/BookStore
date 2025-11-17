b2dc57027c06d2cb9a42de6c65ca9442c4f16ec7
Yan Pai Win <yanpaiwin201916@gmail.com>
2025-11-17 09:07:54 +0630

    ep-68 and custom useTheme hook

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -1,3 +1,11 @@
-export default function useTheme() {
+import { useContext } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
+export default function useTheme() {
+    let contexts=useContext(ThemeContext);
+    if (contexts===undefined) {
+        new Error("useTheme must be used within a ThemeContextProvider");
+    }
+
+    return contexts;
 }
