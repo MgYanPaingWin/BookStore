56c0a60bcc0cc6c0db113c149c4add489e1afc10
Yan Pai Win <yanpaiwin201916@gmail.com>
2025-11-19 15:03:02 +0630

    setup useReducer in ThemeContext

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,11 +1,25 @@
 //themecontext
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 const ThemeContext = createContext();
 
+let ThemeReducer=(state,action)=>{
+  console.log(state,action)
+}
+
 //themecontextprovider component
 const ThemeContextProvider = ({ children }) => {
-  return <ThemeContext.Provider value={{theme : 'dark'}}>
+
+  let [state,dispatch]=useReducer(ThemeReducer,{ 
+    theme : 'light'
+  })
+
+  let changeTheme=()=>{
+    //action-> type + palyload -> {type,payload}
+    dispatch({type : "CHANGE_THEME",payload : 'dark'})
+  }
+
+  return <ThemeContext.Provider value={{...state,changeTheme}}>
     {children}
   </ThemeContext.Provider>;
 };
