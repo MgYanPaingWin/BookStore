eba43d0732e18a9310418e02f98db73903cc7d3b
Yan Pai Win <yanpaiwin201916@gmail.com>
2025-11-26 08:07:05 +0630

    add firebase file

---
+++ src/firebase/index.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAKyDtAx3dn9MVI_y_TzLbl9QbcmzffUJw",
+  authDomain: "library-app-45b03.firebaseapp.com",
+  projectId: "library-app-45b03",
+  storageBucket: "library-app-45b03.firebasestorage.app",
+  messagingSenderId: "878155836952",
+  appId: "1:878155836952:web:fd76f2979ee65c0c555d6d",
+  measurementId: "G-2X262FQEY8"
+};
+
+const app = initializeApp(firebaseConfig);
